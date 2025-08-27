f577748c2f98fb4897e2fb4168ef0895dc72a4ea
KKH0208 <bc2430@naver.com>
2025-08-27 15:12:50 +0900

    test

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -1,3 +1,6 @@
+# 메일은 일단 실패했고 スラック로 보내는법 찾아보자 훨씬 간단한 듯 하다. 
+
+
 #!/bin/bash
 # monitor.sh - 서버 리소스 모니터링 및 알림
 # cpu/mem/disk사용량 확인하고 임계치 이상이면 알람 보내는 스크립트 
@@ -8,6 +11,18 @@
 SCRIPT_DIR=$(cd "$(dirname $0)/../" && pwd )
 TIMESTAMP=$(date "+%y%m%d_%H%M%S")
 LOG_FILE=$SCRIPT_DIR/reports/monitor_$TIMESTAMP.log
+
+
+# 알리고자 하는 채널
+CHANNEL="#server-notification"
+
+# 채널 내에서 나오게 될 사용자 이름
+USERNAME="Bash Webhook bot"
+
+# 채널에 사용하고자 하는 아이콘
+EMOJI=":heart:"
+
+
 
 
 CPU_THRESHOLD=10
@@ -43,7 +58,15 @@ monitor_server(){
     fi 
 }
 
+send_alert(){
+    MSG=$1
+    PAYLOAD="payload={\"channel\": \"$CHANNEL\", \"username\": \"$USERNAME\", \"text\": \"$MSG\", \"icon_emoji\": \"$EMOJI\"}"
+    HOOK=https://hooks.slack.com/services/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
+
+    curl -X POST --data-urlencode "$PAYLOAD" "$HOOK"
+}
 #========== 메인 ============
 
 
 monitor_server
+send_alert "test"
