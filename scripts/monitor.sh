dd23f40cbcac138978b45f3b2cb152c5073d7e9a
KKH0208 <bc2430@naver.com>
2025-08-27 15:24:41 +0900

    test

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -12,19 +12,6 @@ SCRIPT_DIR=$(cd "$(dirname $0)/../" && pwd )
 TIMESTAMP=$(date "+%y%m%d_%H%M%S")
 LOG_FILE=$SCRIPT_DIR/reports/monitor_$TIMESTAMP.log
 
-
-# 알리고자 하는 채널
-CHANNEL="#server-notification"
-
-# 채널 내에서 나오게 될 사용자 이름
-USERNAME="Bash Webhook bot"
-
-# 채널에 사용하고자 하는 아이콘
-EMOJI=":heart:"
-
-
-
-
 CPU_THRESHOLD=10
 MEM_THRESHOLD=15
 DISK_THRESHOLD=10
@@ -45,28 +32,31 @@ monitor_server(){
     CPU=$(top -bn 1 | grep "%Cpu" | awk '{print int($2+$4)}')
     if [ $CPU -gt $CPU_THRESHOLD ]; then 
         log "[WARN]" "Cpu 사용량이 높습니다."
+        send_alert "서버 Cpu 사용량이 높습니다. 확인 부탁드립니다."
     fi 
 
     MEM=$(free | awk '/Mem:/ {printf( "%d",$3/$2*100)}')
     if [ $MEM -gt $MEM_THRESHOLD ]; then 
         log "[WARN]" "Memory 사용량이 높습니다." 
+        send_alert "서버 메모리 사용량이 높습니다. 확인 부탁드립니다."
+
     fi 
 
     DISK=$(df -h / | awk ' NR==2 {gsub(/%/,"",$5); print $5}') 
     if [ $DISK -gt $DISK_THRESHOLD ]; then 
         log "[WARN]" "Disk 사용량이 높습니다." 
+        send_alert "서버 디스크 사용량이 높습니다. 확인 부탁드립니다."
+
     fi 
 }
 
 send_alert(){
     MSG=$1
-    PAYLOAD="payload={\"channel\": \"$CHANNEL\", \"username\": \"$USERNAME\", \"text\": \"$MSG\", \"icon_emoji\": \"$EMOJI\"}"
-    HOOK=https://hooks.slack.com/services/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
-
-    curl -X POST --data-urlencode "$PAYLOAD" "$HOOK"
+    curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"$MSG\"}" https://hooks.slack.com/services/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
+    
 }
 #========== 메인 ============
 
 
 monitor_server
-send_alert "test"
+
