2e332c38ccf62e5938d9f6d9cd289528880e8353
KKH0208 <bc2430@naver.com>
2025-08-30 15:18:19 +0900

    U_07 ver1.3

--- scripts/security_audit.sh
+++ scripts/security_audit.sh
@@ -237,7 +237,7 @@ U_07(){
     if [ -f /etc/passwd ]; then 
 
         check=$(find /etc/passwd -type f ! -perm 643 | wc -l)
-        if [ check -eq 1 ]; then 
+        if [ $check -eq 1 ]; then 
             log "WARN" "/etc/passwd파일의 권한이 큽니다"
             log "WARN" "U_07테스트 결과 취약" 
         else
