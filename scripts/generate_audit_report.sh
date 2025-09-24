c4e1d0d31c1a368330b417b486ff4e9cb79994c9
KKH0208 <bc2430@naver.com>
2025-09-25 03:42:41 +0900

    generate_audit_report.sh Ver 2.52

--- scripts/generate_audit_report.sh
+++ scripts/generate_audit_report.sh
@@ -84,8 +84,8 @@ create_vuln_action_plan(){
                 subkeys=(${error_code_dict[$item]}) #문자열이니까 일단 배열로 만들어주고 쓰자.
                 for ((i=0;i<error_code_len;i++)); do 
                     echo "$item  $subkeys[$i]"
-                    desc=$(jq -r --arg k "$item" --arg sk "$subkeys[$i]" '.[$k][$sk].desc' "$json_file")
-                    action=$(jq -r --arg k "$item" --arg sk "$subkeys[$i]" '.[$k][$sk].action // ""' "$json_file")
+                    desc=$(jq -r --arg k "$item" --arg sk "${subkeys[$i]}" '.[$k][$sk].desc' "$json_file")
+                    action=$(jq -r --arg k "$item" --arg sk "${subkeys[$i]}" '.[$k][$sk].action // ""' "$json_file")
                     write_md "- 상황: $desc"
                     write_md "- 조치: $action"
                     write_md " "
