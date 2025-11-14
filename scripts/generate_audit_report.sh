20c30375beff2220f9a22b3bf59ffdd9eafac2bd
kkh0208 <bc2430@naver.com>
2025-11-14 12:50:03 +0900

    hi

--- scripts/generate_audit_report.sh
+++ scripts/generate_audit_report.sh
@@ -19,12 +19,12 @@ csv_file="../config/data_for_report.csv"
 json_file="../config/error_code_table.json"
 pass_ratio=$(echo "scale=2; ($pass_cnt / 39 ) * 100" | bc)
 na_ratio=$(echo "scale=2; ($na_cnt / 39 ) * 100" | bc)
-fail_ratio=$(echo "scale=2; ($fail_cnt / 39 ) * 100" | bc)
+fail_ratio=$(echo "scale=2; 100 - $pass_ratio - $na_ratio" | bc)
 
 #========== 함수 ============
 
 write_md(){
-    echo "$1  " >> $report
+    echo "$1<br>" >> $report
 }
 
 
@@ -67,20 +67,22 @@ create_audit_result_summary() {
 
 create_audit_result_detail(){
     write_md "## 3. 상세 점검 결과"
+    write_md "39가지 보안 점검 사항에 대해 평가기준(양호, 경고, 취약)을 정의하고, 이를 기반으로 수행된 최종 점검 결과를 표시한다"
     while IFS=',' read -r no title check_criteria pass fail na
     do 
         write_md "### U-$no $title"
         write_md "#### 점검 기준"
-        write_md "$check_criteria"
+        write_md "* $check_criteria"
         write_md "#### 양호"
-        write_md "$pass"
+        write_md "* $pass"
         write_md "#### 경고"
-        write_md "$na"
+        write_md "* $na"
         write_md "#### 취약"
-        write_md "$fail"
+        write_md "* $fail"
         write_md "#### 점검 결과"
-        write_md "${audit_result[$no]}"
+        write_md "* ${audit_result[$no]}"
         write_md " "
+        write_md " --- "
 
     done < "$csv_file"
 
@@ -109,9 +111,9 @@ create_vuln_action_plan(){
                     desc=$(jq -r --arg k "$item" --arg sk "${subkeys[$i]}" '.[$k][$sk].desc' "$json_file")
                     action=$(jq -r --arg k "$item" --arg sk "${subkeys[$i]}" '.[$k][$sk].action // ""' "$json_file")
                     write_md "#### 현황"
-                    write_md "$desc"
+                    write_md "* $desc"
                     write_md "#### 조치"
-                    write_md "$action"
+                    write_md "* $action"
                     write_md " "
 
                 done 
@@ -122,9 +124,9 @@ create_vuln_action_plan(){
                 desc=$(jq -r --arg k "$item" --arg sk "$subkey" '.[$k][$sk].desc' "$json_file")
                 action=$(jq -r --arg k "$item" --arg sk "$subkey" '.[$k][$sk].action // ""' "$json_file")
                 write_md "#### 현황"
-                write_md "$desc"
+                write_md "* $desc"
                 write_md "#### 조치"
-                write_md "$action"
+                write_md "* $action"
                 write_md " "
 
                 if [[ "$item" == "U_13" || "$item" == "U_14" || "$item" == "U_21" || "$item" == "U_22" || "$item" == "U_23" || "$item" == "U_26" || "$item" == "U_27" || "$item" == "U_28" || "$item" == "U_35" ]]; then 
