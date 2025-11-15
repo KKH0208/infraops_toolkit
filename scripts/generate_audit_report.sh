bdd9ff697d4eeb5b9fc9ce67bf5f0029856c0bf2
kkh0208 <bc2430@naver.com>
2025-11-15 15:55:30 +0900

    hi

--- scripts/generate_audit_report.sh
+++ scripts/generate_audit_report.sh
@@ -30,16 +30,29 @@ write_md(){
 
 
 create_header() {
-    echo "---" >>$report
-    write_md "# 서버 보안 감사 보고서"
-    write_md "**작성일:** $(date '+%y-%m-%d')"
-    write_md "**점검 대상:** EC2 Apache 서버 (IP: 192.168.0.1)"
-    write_md "**작성자:** ${USER}"
-    write_md " "
-    echo "\newpage" >>$report
-    echo "---" >>$report
+    echo '---' >>$report
+    echo 'title: "서버 보안 감사 보고서"' >>$report
+    echo 'author: "**작성자:** ${USER}"' >>$report
+    echo 'subtitle: "점검 대상: EC2 Apache 서버 (IP: 192.168.0.1)"' >>$report
+    echo 'date: "**작성일:** $(date '+%y-%m-%d')"' >>$report
+    echo 'fontsize: 14pt ' >>$report
+    echo '---' >>$report
+    echo '\newpage' >>$report
+
+
+
+    # write_md "# 서버 보안 감사 보고서"
+    # write_md "**작성일:** $(date '+%y-%m-%d')"
+    # write_md "**점검 대상:** EC2 Apache 서버 (IP: 192.168.0.1)"
+    # write_md "**작성자:** ${USER}"
+    # write_md " "
+    # echo "\newpage" >>$report
+    # echo "---" >>$report
 
 }
+
+
+
 
 create_audit_purpose() {
     write_md "## 1. 개요"
